353d68372e6d7721d11d7a7cba4f57464e1655c0
molinajuan <molina.juanignacio@gmail.com>
2021-02-02 08:47:51 -0300

    subiendo archivos a github

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const publicPath = path.resolve("public");
+app.use ( express.static(publicPath) );
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3030, () => {
+    console.log('Servidor corriendo en el puerto 3030');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve("./views/home.html"));
+});
+
+app.post('/',(req,res)=>{
+
+    // validar usuario y contraseña
+    
+    // si esta mal, volver a login y mostrar errores
+
+    // si esta bien , loguear y redireccionar al hom
+
+    res.send(req.body);
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve("./views/register.html"));
+});
+
+app.post('/register',(req,res) => {
+    res.send(req.body);
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve("./views/login.html"));
+});
+
+app.post('/login',(req,res) => {
+    res.send(req.body);
+});
